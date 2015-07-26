75f2eac97040087668968ea917e893f00027d091
Jack Ketcham <kcajketcham@gmail.com>
2015-07-25 20:18:05 -0700

    Add initial dashboard, page & navigation components

---
+++ src/js/components/page-container.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var h = require('react-hyperscript');
+var React = require('react');
+
+var PageContainer = React.createClass({
+
+  render: function render() {
+    return(
+      h('div', '')
+    );
+  }
+
+});
+
+module.exports = PageContainer;
