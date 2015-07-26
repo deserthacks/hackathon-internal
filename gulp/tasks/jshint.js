3ade5c04dd30f3032c6705b02b8a623d008fe94c
Jack Ketcham <kcajketcham@gmail.com>
2015-07-25 20:12:22 -0700

    Update gulp tasks; less, jshint

---
+++ gulp/tasks/jshint.js
@@ -0,0 +1,9 @@
+var jshint = require('gulp-jshint');
+var gulp = require('gulp');
+var config = require('../config').lint;
+
+gulp.task('lint', function() {
+  return gulp.src(config.src)
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 var connect = require('gulp-connect');
-var config = require('../config.js').sass;
+var config = require('../config.js').less;
+var less = require('gulp-less');
+var minifyCSS = require('gulp-minify-css');
 
-gulp.task('styles', function() {
-  gulp.src(config.src)
-    .pipe(sass(config.settings))
+gulp.task('styles', function () {
+  return gulp.src(config.src)
+    .pipe(less(config.settings))
+    .pipe(minifyCSS())
     .pipe(gulp.dest(config.dest))
     .pipe(connect.reload());
 });
